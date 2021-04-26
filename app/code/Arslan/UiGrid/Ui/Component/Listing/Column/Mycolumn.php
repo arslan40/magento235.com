98146e9992b019181f8b73309baa3fa693ad1745
Muhammad Arslan <arslant40@gmail.com>
2021-04-26 16:20:17 +0500

    custom column added to order grid

---
+++ app/code/Arslan/UiGrid/Ui/Component/Listing/Column/Mycolumn.php
@@ -0,0 +1,44 @@
+<?php
+namespace Arslan\UiGrid\Ui\Component\Listing\Column;
+ 
+use \Magento\Sales\Api\OrderRepositoryInterface;
+use \Magento\Framework\View\Element\UiComponent\ContextInterface;
+use \Magento\Framework\View\Element\UiComponentFactory;
+use \Magento\Ui\Component\Listing\Columns\Column;
+use \Magento\Framework\Api\SearchCriteriaBuilder;
+ 
+class Mycolumn extends Column
+{
+ 
+    protected $_orderRepository;
+    protected $_searchCriteria;
+    protected $_customfactory;
+ 
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        OrderRepositoryInterface $orderRepository,
+        SearchCriteriaBuilder $criteria,
+        array $components = [], array $data = [])
+    {
+        $this->_orderRepository = $orderRepository;
+        $this->_searchCriteria  = $criteria;
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+    }
+ 
+    public function prepareDataSource(array $dataSource)
+    {
+        if (isset($dataSource['data']['items'])) 
+        {
+            foreach ($dataSource['data']['items'] as & $item) 
+            {
+                $order  = $this->_orderRepository->get($item["entity_id"]);
+                $order_id = $order->getEntityId();
+            //   var_dump($this->getData('name'));
+                $item["order_status"] = $order->getstate();
+                $item["telephone"] = $order->getShippingAddress()->gettelephone();
+            }
+        }
+        return $dataSource;
+    }
+}
